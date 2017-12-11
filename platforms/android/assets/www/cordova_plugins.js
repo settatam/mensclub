eccfc0dd2dbbb7f875c36eeceb15db7114fdf547
Seth Atam <sethatam@Seths-MacBook-Pro.local>
2017-12-11 08:57:43 -0500

    Add whitelist

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -237,7 +237,8 @@ module.exports.metadata =
     "cordova-plugin-media-capture": "1.4.3",
     "cordova-plugin-network-information": "1.3.4",
     "cordova-plugin-vibration": "2.1.6",
-    "cordova-plugin-splashscreen": "4.1.0"
+    "cordova-plugin-splashscreen": "4.1.0",
+    "cordova-plugin-whitelist": "1.3.3"
 };
 // BOTTOM OF METADATA
 });
